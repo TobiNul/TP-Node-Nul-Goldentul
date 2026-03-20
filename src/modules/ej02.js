24741bc35da5862ac8623492537b47e53d209e49
TOMAS ARIEL GOLDENTUL <49319223@ort>
2026-03-20 09:41:45 -0300

    Terminado ej2

---
+++ src/modules/ej02.js
@@ -0,0 +1,7 @@
+import {PI, sumar} from './matematica.js';
+
+let total, numero1 = 10, numero2= 20;
+console.clear();
+console.log(`La constante PI vale '${PI}'`);       
+total = sumar(numero1, numero2); 
+console.log(`sumar(${numero1}, ${numero2}) =  ${total}`);
---
+++ src/modules/matematica.js
@@ -0,0 +1,12 @@
+function sumar (numero1, numero2){
+return numero1 + numero2;
+} 
+function restar (numero1, numero2){
+return numero1 - numero2;
+}
+const multiplicar = (numero1, numero2) => numero1 * numero2;
+const dividir = (numero1, numero2) => numero1 / numero2;
+const PI = 3.14;
+const numeros = ["dos", "cuatro", "ocho", "diez"];  
+export {PI, sumar, multiplicar, restar, dividir, numeros};
+
