8e52f289f8a20b5df09ce5d1f65661044a87e2cc
TOBÍAS ILÁN NUL <49194485@ort>
2026-03-27 08:18:08 -0300

    resultado ej08 con await

--- src/modules/ej08.js
+++ src/modules/ej08.js
@@ -120,6 +120,25 @@ const OMDBGetByImdbID = async (imdbID) => {
 
 };
 
-
-
 export {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID};
+
+import
+{
+  OMDBSearchByPage,
+  OMDBSearchComplete,
+  OMDBGetByImdbID
+} from "./modules/omdb-wrapper.js";
+
+let resultado = null;
+
+//1
+resultado = await OMDBSearchByPage("cars", 1);
+console.log("OMDBSearchByPage", resultado);
+
+//2
+resultado = await OMDBSearchComplete("cars");
+console.log("OMDBSearchComplete", resultado);
+
+//3
+resultado = await OMDBGetByImdbID("tt0317219");
+console.log("OMDBGetByImdbID", resultado);
